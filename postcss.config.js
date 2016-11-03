2d1d8706a87ed0b8816204ab4e21d8ff914aaac1
景天 <jingtian@naitang.com>
2016-11-03 13:50:31 +0800

    page

--- postcss.config.js
+++
@@ -1,6 +0,0 @@
-var autoprefixer = require('autoprefixer')
-var browserslist = require('browserslist')
-
-module.exports = {
-    plugins: [autoprefixer({browsers: browserslist('last 2 version, > 0.1%')})]
-}
